504b544d106bccba3ad73217c1ffdd11ceb51e34
Leonardo Fontes <leofontes@gmail.com>
2020-01-24 14:06:58 -0300

    parcial resultscreen

--- src/screens/Result/ResultScreen.tsx
+++ src/screens/Result/ResultScreen.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
 import SafeArea from '../../components/basic/SafeArea'
-import {StyleSheet, View} from 'react-native'
+import {Animated, StyleSheet, View} from 'react-native'
 import Colors from '../../utils/colors'
 import BasicText from '../../components/basic/BasicText'
 import {useSelector} from 'react-redux'
@@ -10,8 +10,16 @@ import ResultList from '../../components/compound/ResultList'
 import Button from '../../components/basic/Button'
 import {useNavigation} from 'react-navigation-hooks'
 import ConfettiCannon from 'react-native-confetti-cannon'
+import CardView from "../../components/basic/CardView"
 
 export default () => {
+  const [contentOpacity, setContentOpacity] = useState<Animated.AnimatedValue>(
+    new Animated.Value(0),
+  )
+  const [loadingCardOpacity, setLoadingCardOpacity] = useState<
+    Animated.AnimatedValue
+  >(new Animated.Value(1))
+
   const {questions} = useSelector((state: ApplicationState) => state.quiz)
   const {navigate} = useNavigation()
 
@@ -21,24 +29,34 @@ export default () => {
 
   return (
     <SafeArea>
-      <View style={styles.scoreContainer}>
-        <BasicText center size={22} color={Colors.blueAqua}>
-          {getScoreString(questions)}
-        </BasicText>
-      </View>
+      <Animated.View style={{opacity: loadingCardOpacity, flex: 1, justifyContent: 'center'}}>
+        <CardView style={styles.card}>
+          <BasicText center color={Colors.pink} light>
+            Let's see how you did!
+          </BasicText>
+        </CardView>
+      </Animated.View>
 
-      <View style={styles.content}>
-        <ResultList questions={questions} />
-      </View>
-      <Button
-        label="PLAY AGAIN?"
-        onPress={onPlayAgainButtonPress}
-        labelColor={Colors.blueAqua}
-        bgColor={Colors.black}
-        width={200}
-      />
+      <Animated.View style={{opacity: contentOpacity, flex: 1}}>
+        <View style={styles.scoreContainer}>
+          <BasicText center size={22} color={Colors.blueAqua}>
+            {getScoreString(questions)}
+          </BasicText>
+        </View>
 
-      <ConfettiCannon count={200} fadeOut={true} origin={{x: 0, y: 0}} />
+        <View style={styles.content}>
+          <ResultList questions={questions} />
+        </View>
+        <Button
+          label="PLAY AGAIN?"
+          onPress={onPlayAgainButtonPress}
+          labelColor={Colors.blueAqua}
+          bgColor={Colors.black}
+          width={200}
+        />
+      </Animated.View>
+
+      {/*<ConfettiCannon count={200} fadeOut={true} origin={{x: 0, y: 0}} />*/}
     </SafeArea>
   )
 }
@@ -48,12 +66,16 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 
+  card: {
+    margin: 16,
+    padding: 16,
+  },
+
   scoreContainer: {
     backgroundColor: Colors.black,
     justifyContent: 'center',
     paddingVertical: 8,
   },
-
   content: {
     flex: 1,
     margin: 16,
